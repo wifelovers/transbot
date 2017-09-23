2710ae365125565cdc590aefb9fe3fa856b25b0a
deathisabove <deathisabove@gmail.com>
2017-09-23 16:37:04 +0700

    Replybot example

---
+++ replybot.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"gopkg.in/telegram-bot-api.v4"
+)
+func main() {
+	bot, err := tgbotapi.NewBotAPI("#our_api_key")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+}
